fddf19f94a3978db91a0b3527a7a26b4c961c90a
Shubham Singh <shubhams12333@gmail.com>
2025-07-01 19:21:48 +0530

    implement crud apis

--- task-manager-backend/app.py
+++ task-manager-backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
@@ -35,28 +35,48 @@ def create_app(database_uri_override=None):
 
     @app.route('/tasks', methods=['POST'])
     def create_task():
-        raise NotImplementedError("Not implemented")
-
+        data = request.get_json()
+        new_task = Task(title=data['title'], description=data.get('description', ''))
+        db.session.add(new_task)
+        db.session.commit()
+        return jsonify({'message': 'New task created!'}), 201
 
     @app.route('/tasks', methods=['GET'])
     def get_tasks():
-        raise NotImplementedError("Not implemented")
-        
+        tasks = Task.query.all()
+        output = []
+        for task in tasks:
+            task_data = {'id': task.id, 'title': task.title, 'description': task.description}
+            output.append(task_data)
+        return jsonify({'tasks': output})
 
     @app.route('/tasks/<task_id>', methods=['GET'])
     def get_task(task_id):
-        raise NotImplementedError("Not implemented")
-        
+        task = db.session.get(Task, task_id)
+        if task is None:
+            abort(404)
+        task_data = {'id': task.id, 'title': task.title, 'description': task.description}
+        return jsonify(task_data)
 
     @app.route('/tasks/<task_id>', methods=['PUT'])
     def update_task(task_id):
-        raise NotImplementedError("Not implemented")
-        
+        task = db.session.get(Task, task_id)
+        if task is None:
+            abort(404)
+        data = request.get_json()
+        task.title = data['title']
+        task.description = data.get('description', task.description)
+        db.session.commit()
+        return jsonify({'message': 'Task updated!'})
 
     @app.route('/tasks/<task_id>', methods=['DELETE'])
     def delete_task(task_id):
-        raise NotImplementedError("Not implemented")
-        
+        task = db.session.get(Task, task_id)
+        if task is None:
+            abort(404)
+        db.session.delete(task)
+        db.session.commit()
+        return jsonify({'message': 'Task deleted!'})
 
     return app
 
